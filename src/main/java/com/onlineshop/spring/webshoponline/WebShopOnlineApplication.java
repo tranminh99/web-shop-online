953dd59fc73ecf21a63b0a57a159c3819f13e67e
Tran Binh Minh <tranminh99tb@gmail.com>
2023-11-12 17:01:58 +0700

    first commit

---
+++ src/main/java/com/onlineshop/spring/webshoponline/WebShopOnlineApplication.java
@@ -0,0 +1,13 @@
+package com.onlineshop.spring.webshoponline;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+
+@SpringBootApplication
+public class WebShopOnlineApplication {
+
+	public static void main(String[] args) {
+		SpringApplication.run(WebShopOnlineApplication.class, args);
+	}
+
+}
---
+++ src/test/java/com/onlineshop/spring/webshoponline/WebShopOnlineApplicationTests.java
@@ -0,0 +1,13 @@
+package com.onlineshop.spring.webshoponline;
+
+import org.junit.jupiter.api.Test;
+import org.springframework.boot.test.context.SpringBootTest;
+
+@SpringBootTest
+class WebShopOnlineApplicationTests {
+
+	@Test
+	void contextLoads() {
+	}
+
+}
